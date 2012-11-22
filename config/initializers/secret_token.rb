51946840efd5d1cd823ee281a4d89e845d0b05df
eicca <wtltl2@gmail.com>
2012-11-22 12:46:58 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeacherHelper::Application.config.secret_token = '22feaf1b2044ae5f3c4a70232f259b5231a64d4573856163c08defe64ad16045f146d0db53b64bb58650ee5dda1e2979c4ab61a48929efebeb2edf58f580b561'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TeacherHelper::Application.config.session_store :cookie_store, key: '_teacher-helper_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TeacherHelper::Application.config.session_store :active_record_store
