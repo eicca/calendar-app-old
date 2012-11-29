4a4ad8f9cac2071c4d16a03e73284b2790b6a792
eicca <wtltl2@gmail.com>
2012-11-29 19:21:17 +0400

    added inherited_resources

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,16 +1,4 @@
-class TeachersController < ApplicationController
+class TeachersController < InheritedResources::Base
   load_and_authorize_resource
   respond_to :html, :json
-
-  def update
-    respond_to do |format|
-      if @teacher.update_attributes(params[:teacher])
-        format.html { redirect_to @teacher, notice: 'teacher was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @teacher.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 end
--- db/migrate/20121127195630_add_schedule_to_teachers.rb
+++
@@ -1,5 +0,0 @@
-class AddScheduleToTeachers < ActiveRecord::Migration
-  def change
-    add_column :teachers, :schedule, :text
-  end
-end
