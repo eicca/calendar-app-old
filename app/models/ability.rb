51112f919461e82ffe8c91b741e517c21804a14f
eicca <wtltl2@gmail.com>
2012-11-30 01:54:12 +0400

    changed manipulation to select

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,36 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= Student.new # gues user
-
-    can :read, :all
+    user ||= Student.new # guest user
 
     if user.kind_of? Teacher
       can :manage, Teacher, id: user.id
     elsif user.kind_of? Student
       can :manage, Student, id: user.id
     end
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    can :read, :all
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 Teacher.create! name: 'Andy', email: 'test@test.com', password: 'razdva'
 Teacher.create! name: 'Iwan', email: 'test2@test.com', password: 'razdva'
+
+Student.create! name: 'student', email: 'test3@test.com', password: 'razdva'
